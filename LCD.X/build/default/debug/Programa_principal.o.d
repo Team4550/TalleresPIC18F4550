7cc5643721965bcf694fdbc81bea42238085b46e
Wels <williams.limonchi.sandoval@gmail.com>
2020-05-30 11:31:02 -0500

    LCD 4 bits y 8 bits

---
+++ LCD.X/build/default/debug/Programa_principal.o.d
@@ -0,0 +1,4 @@
+build/default/debug/Programa_principal.o:  \
+	D:/Electronica/4550/Taller-Pic18f4550/LCD.X/Programa_principal.asm  \
+	D:/Electronica/4550/Taller-Pic18f4550/LCD.X/LCD_4bits.inc  \
+
---
+++ LCD_4bits.X/build/default/production/Programa_principal.o.d
@@ -0,0 +1,4 @@
+build/default/production/Programa_principal.o:  \
+	D:/Electronica/4550/Taller-Pic18f4550/LCD_4bits.X/Programa_principal.asm  \
+	D:/Electronica/4550/Taller-Pic18f4550/LCD_4bits.X/LCD_4bits.inc  \
+
